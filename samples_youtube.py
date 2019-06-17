59fdc73b3f44b62462b8622899c995ff5a55ae7d
Daniel Watson <daniel.watson@nyu.edu>
2019-06-17 10:27:25 -0700

    Parse youtube video transcripts to separate file

--- samples_youtube.py
+++ samples_youtube.py
@@ -20,36 +20,35 @@ if __name__ == "__main__":
     video_file = open(os.path.join("data", "youtube_video_samples.txt"), "w")
     comment_file = open(os.path.join("data", "youtube_comment_samples.txt"), "w")
 
-    with open(os.path.join("data", "youtube_samples.txt"), "w") as f:
-        acc = []
-        for channel in channels[:num_channels]:
-            channel_files = [c for c in os.listdir(path) if c.startswith(channel)]
+    acc = []
+    for channel in channels[:num_channels]:
+        channel_files = [c for c in os.listdir(path) if c.startswith(channel)]
 
-            for cf in channel_files:
-                df = pd.read_csv(os.path.join(path, cf))
-                # 'index', 'video_id', 'video_url', 'video_title', 'channel', 'series',
-                # 'video_snippet', 'video', 'comment', 'reply', 'video_op',
-                # 'comment_op', 'reply_op', 'date_posted', 'content', 'date_scraped'
-                for row in df.iterrows():
-                    row = row[1]
-                    if int(row["video"]) == 1:
-                        video_file.write(
-                            " ".join(utils.tokenize(str(row["content"]))) + "\n"
-                        )
-                    else:
-                        acc.append(row["content"])
-
-                    if len(acc) >= num_videos_per_channel:
-                        break
+        for cf in channel_files:
+            df = pd.read_csv(os.path.join(path, cf))
+            # 'index', 'video_id', 'video_url', 'video_title', 'channel', 'series',
+            # 'video_snippet', 'video', 'comment', 'reply', 'video_op',
+            # 'comment_op', 'reply_op', 'date_posted', 'content', 'date_scraped'
+            for row in df.iterrows():
+                row = row[1]
+                if int(row["video"]) == 1:
+                    video_file.write(
+                        " ".join(utils.tokenize(str(row["content"]))) + "\n"
+                    )
+                else:
+                    acc.append(row["content"])
 
                 if len(acc) >= num_videos_per_channel:
                     break
 
             if len(acc) >= num_videos_per_channel:
-                for comment in acc:
-                    comment_file.write(" ".join(utils.tokenize(str(comment))) + "\n")
-                acc = []
-                continue
+                break
+
+        if len(acc) >= num_videos_per_channel:
+            for comment in acc:
+                comment_file.write(" ".join(utils.tokenize(str(comment))) + "\n")
+            acc = []
+            continue
 
     video_file.close()
-    commment_file.close()
+    comment_file.close()

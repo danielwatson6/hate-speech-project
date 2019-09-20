5da28c69d125f0929c21bd346c4eb2ff9981bd0e
Daniel Watson <daniel.watson@nyu.edu>
2019-09-20 11:55:33 +0400

    Added missing temporal reduction

--- models/mf_doc2vec.py
+++ models/mf_doc2vec.py
@@ -20,14 +20,22 @@ def mish(x):
 class MF_Doc2Vec(MoralFoundations):
     default_hparams = {
         **MoralFoundations.default_hparams,
+        "reduction": "mean",
         "hidden_sizes": [512],
         "dropout": 0.0,
     }
 
     def make_encoder(self):
         encoder = tf.keras.Sequential()
+
+        if self.hparams.reduction == "max":
+            encoder.add(tfkl.Lambda(lambda x: tf.reduce_max(x, axis=1)))
+        else:
+            encoder.add(tfkl.Lambda(lambda x: tf.reduce_mean(x, axis=1)))
+
         for hidden_size in self.hparams.hidden_sizes:
             encoder.add(tfkl.Dense(hidden_size, activation=mish))
             if self.hparams.dropout > 0:
                 encoder.add(tfkl.Dropout(self.hparams.dropout))
+
         return encoder

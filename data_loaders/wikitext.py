9f1a9a63293436e068dfb57cb744c7c7e65f91ad
Daniel Watson <daniel.watson@nyu.edu>
2020-01-26 17:39:24 +0400

    fix #9

--- data_loaders/wikitext.py
+++ data_loaders/wikitext.py
@@ -3,11 +3,18 @@ import os
 import tensorflow as tf
 
 import boilerplate as tfbp
+from data_loaders import utils
 
 
 @tfbp.default_export
 class WikiText(tfbp.DataLoader):
-    default_hparams = {"batch_size": 32, "corpus": 2, "max_seq_len": 40}
+    default_hparams = {
+        "batch_size": 32,
+        "corpus": 2,
+        "max_seq_len": 40,
+        "punctuation": True,
+        "lowercase": True,
+    }
 
     def call(self):
         if self.hparams.corpus == 103:
@@ -17,59 +24,31 @@ class WikiText(tfbp.DataLoader):
         else:
             raise ValueError("`corpus` hyperparameter can only attain values 2 or 103.")
 
-        vocab_path = os.path.join(data_path, "wiki.vocab.tsv")
+        vocab_path = os.path.join(data_path, "wiki.vocab")
+        embeds_path = os.path.join(data_path, "wiki.vocab")
 
-        # Args: filename, key_dtype, key_index, value_dtype, value_index, vocab_size
-        word_to_id_init = tf.lookup.TextFileInitializer(
-            vocab_path,
-            tf.string,
-            0,
-            tf.int64,
-            tf.lookup.TextFileIndex.LINE_NUMBER,
-            vocab_size=self.hparams.vcab_size,
+        # Used by models to display outputs as strings; the conversion is data-dependent.
+        self.word_to_id, self.id_to_word = utils.make_word_id_maps(
+            vocab_path, self.hparams.vocab_size
         )
-        id_to_word_init = tf.lookup.TextFileInitializer(
-            vocab_path,
-            tf.int64,
-            tf.lookup.TextFileIndex.LINE_NUMBER,
-            tf.string,
-            0,
-            vocab_size=self.hparams.vocab_size,
+
+        # Used by models to find the initial values of the embedding matrix, which are
+        # data-dependent.
+        self.embedding_matrix = utils.save_or_load_embeds(
+            embeds_path, vocab_path, self.hparams.vocab_size
         )
-        self.word_to_id = tf.lookup.StaticHashTable(word_to_id_init, 1)
-        self.id_to_word = tf.lookup.StaticHashTable(id_to_word_init, "<unk>")
 
         if self.method == "fit":
-            train_inputs = tf.data.TextLineDataset(
-                os.path.join(data_path, "wiki.train.inputs")
+            train_dataset = self._make_dataset(
+                os.path.join(data_path, "wiki.train.clean"), shuffle=10000
             )
-            train_labels = tf.data.TextLineDataset(
-                os.path.join(data_path, "wiki.train.labels")
+            valid_dataset = self._make_dataset(
+                os.path.join(data_path, "wiki.valid.clean"), shuffle=10000
             )
-            valid_inputs = tf.data.TextLineDataset(
-                os.path.join(data_path, "wiki.valid.inputs")
-            )
-            valid_labels = tf.data.TextLineDataset(
-                os.path.join(data_path, "wiki.valid.labels")
-            )
-            train_dataset = tf.data.Dataset.zip((train_inputs, train_labels))
-            valid_dataset = tf.data.Dataset.zip((valid_inputs, valid_labels))
-
-            train_dataset = self._transform_dataset(train_dataset)
-            valid_dataset = self._transform_dataset(valid_dataset)
-
             return train_dataset, valid_dataset
 
         elif self.method == "evaluate":
-            test_inputs = tf.data.TextLineDataset(
-                os.path.join(data_path, "wiki.test.inputs")
-            )
-            test_labels = tf.data.TextLineDataset(
-                os.path.join(data_path, "wiki.test.labels")
-            )
-            test_dataset = tf.data.Dataset.zip((test_inputs, test_labels))
-
-            return self._transform_dataset(test_dataset)
+            return self._make_dataset(os.path.join(data_path, "wiki.test.clean"))
 
         elif self.method == "interact":
 
@@ -81,15 +60,27 @@ class WikiText(tfbp.DataLoader):
             return dataset.map(self._batch_to_ids)
 
     def _batch_to_ids(self, batch):
-        padded = tf.strings.split(batch + " <eos>").to_tensor(default_value="<pad>")
+        sequences = tf.strings.split(batch)
+        # TODO: implement optional lowercasing and punctuation removal.
+        if self.hparams.lowercase:
+            ...
+        if not self.hparams.punctuation:
+            ...
+
+        # Convert the ragged tensor to a regular tensor. This takes care of padding.
+        padded = sequences.to_tensor(default_value="<pad>")
         if self.hparams.max_seq_len:
             padded = padded[:, : self.hparams.max_seq_len]
-        return self.word_to_id.lookup(padded)
+        return self.word_to_id(padded)
 
-    def _transform_dataset(self, dataset):
+    def _make_dataset(self, path, shuffle=None):
+        dataset = tf.data.TextLineDataset(path)
+
+        if shuffle:
+            dataset = dataset.shuffle(shuffle)
+
         dataset = dataset.batch(self.hparams.batch_size)
         dataset = dataset.map(
-            lambda x, y: (self._batch_to_ids(x), self._batch_to_ids(y))
+            self._batch_to_ids, num_parallel_calls=tf.data.experimental.AUTOTUNE
         )
-        dataset = dataset.shuffle(10000)
         return dataset.prefetch(1)

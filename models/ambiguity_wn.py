540706e0792684ea3029849c772901e7d8584377
Daniel Watson <daniel.watson@nyu.edu>
2020-01-27 14:54:51 +0400

    Fix #10

--- models/ambiguity_wn.py
+++ models/ambiguity_wn.py
@@ -12,4 +12,13 @@ class AmbiguityWordnet(tfbp.Model):
 
     @tfbp.runnable
     def ambiguity(self, data_loader):
-        ...
+        dataset = data_loader()
+        for batch in dataset:
+            batch = data_loader.id_to_word(batch)
+            for seq in batch.numpy():
+                scores = []
+                for word in seq:
+                    word = word.decode("utf8")
+                    score = len(wn.synsets(word))
+                    scores.append(score)
+                print(", ".join(["{:.4f}".format(x) for x in scores]))

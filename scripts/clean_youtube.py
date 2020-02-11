1210ac948dc7699115cdbc44097652d9ed88d0ce
Abdullah Zameek <arz268@nyu.edu>
2020-02-11 17:08:43 +0400

    fix

--- scripts/clean_youtube.py
+++ scripts/clean_youtube.py
@@ -15,13 +15,12 @@ if __name__ == "__main__":
 
     path = os.path.join(os.environ["DATASETS"], "youtube_right")
     channels = list(os.listdir(path))
-    dataset = tf.data.Dataset.from_tensor_slices(channels) 
-    print(dataset)
-    # for channel in channels:
-    #     channel_files = [c for c in os.listdir(path) if c.startswith(channel)]
-    #     for cf in channel_files:
-    #         df = pd.read_csv(os.path.join(path, cf))
-    #         print(df['content'])
+
+    for channel in channels:
+        channel_files = [c for c in os.listdir(path) if c.startswith(channel)]
+        for cf in channel_files:
+            df = pd.read_csv(os.path.join(path, cf))
+            print(df['content'])
 
 
 

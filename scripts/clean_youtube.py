c6e5dcf5b33ec08b9dbe63619fecd524fd721969
Abdullah Zameek <arz268@nyu.edu>
2020-02-13 00:12:14 +0400

    added

--- scripts/clean_youtube.py
+++ scripts/clean_youtube.py
@@ -11,16 +11,27 @@ import utils
 def parse_fn(filename): 
     df = pd.read_csv(filename)
     content = df.pop('content').to_frame()
-    print(content)
+    return content
 
 if __name__ == "__main__":
     path = os.path.join(os.environ["DATASETS"], "youtube_right")
     channels = list(os.listdir(path))
   
-    for channel in channels:    
-        channel_files = [c for c in os.listdir(path) if c.startswith(channel)]
-        for cf in channel_files:
-          parse_fn(os.path.join(path, cf))
+    # for channel in channels:    
+    #     channel_files = [c for c in os.listdir(path) if c.startswith(channel)]
+    #     for cf in channel_files:
+    #       parse_fn(os.path.join(path, cf))
+
+    channel_files = [c for c in os.listdir(path) if c.startswith(channel)]
+    dataset = tf.data.Dataset.from_tensor_slices(channel_files)
+    dataset = dataset.interleave(lambda x: 
+      tf.data.TextLineDataset(x).map(parse_fn, num_parallel_calls=1), 
+      cycle_length=4, block_length=16) 
+    print(dataset)
+    
+
+     
+
           
 
 

4915c739fe67f464bfeed2190559d92f1cf2b748
Abdullah Zameek <arz268@nyu.edu>
2020-02-12 22:10:09 +0400

    added

--- scripts/clean_youtube.py
+++ scripts/clean_youtube.py
@@ -17,7 +17,11 @@ if __name__ == "__main__":
   
     for channel in channels:    
         channel_files = [c for c in os.listdir(path) if c.startswith(channel)]
-        dataset = tf.data.Dataset.from_tensor_slices(channel_files)
+        for cf in channel_files:
+          df = pd.read_csv(os.path.join(path, cf))
+          content = df.pop('content')
+          dataset = tf.data.Dataset.from_tensor_slices(content)
+
         # dataset = dataset.interleave(lambda x: 
         #     tf.data.TextLineDataset(x).map(parse_fn, num_parallel_calls=1), 
         #     cycle_length=4, block_length=16) 

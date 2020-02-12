70335f767c21c55e89306c148646a4bb68e10d9b
Abdullah Zameek <arz268@nyu.edu>
2020-02-13 00:21:22 +0400

    added

--- scripts/clean_youtube.py
+++ scripts/clean_youtube.py
@@ -28,6 +28,8 @@ if __name__ == "__main__":
       tf.data.TextLineDataset(x).map(parse_fn, num_parallel_calls=1), 
       cycle_length=4, block_length=16) 
     print(dataset)
+    for x in dataset:
+      print(x)
 
 
      

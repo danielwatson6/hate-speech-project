584a17082a11ba8cd86455711dd1014a1fdbe853
Abdullah Zameek <arz268@nyu.edu>
2020-02-13 15:11:41 +0400

    added

--- scripts/clean_youtube.py
+++ scripts/clean_youtube.py
@@ -14,7 +14,7 @@ def parse_fn(line):
   defs = [0.] * n_inputs + [tf.constant([], dtype=tf.float32)]
   fields = tf.io.decode_csv(line, record_defaults=defs)
   x = tf.stack(fields[14])
-  print(x.values)
+  print(x.value)
   return x
 
 

0f7209614f33a00b7ff088bedcba1b6a75615fac
Abdullah Zameek <arz268@nyu.edu>
2020-02-15 20:30:26 +0400

    added

--- scripts/clean_youtube.py
+++ scripts/clean_youtube.py
@@ -11,11 +11,15 @@ n_inputs = 16
 n_readers = 5
 
 def parse_fn(line): 
-  defs = [0.] * n_inputs + [tf.constant([], dtype=tf.float32)]
-  fields = tf.io.decode_csv(line, record_defaults=defs)
-  x = tf.stack(fields[14])
-  tf.print(x)
-  return x
+    defs = [0.] * n_inputs + [tf.constant([], dtype=tf.float32)]
+    fields = tf.io.decode_csv(line, record_defaults=defs)
+    x = tf.stack(fields[14])
+    tf.print(x)
+    return x
+
+
+def make_csv_dataset(path):
+    ...
 
 
 if __name__ == "__main__":
@@ -28,15 +32,15 @@ if __name__ == "__main__":
     filepath_dataset = tf.data.Dataset.list_files(channel_paths)
     print(filepath_dataset)
     for x in filepath_dataset:
-      print(x)
+        print(x)
 
     dataset = filepath_dataset.interleave(
-        lambda filepath: tf.data.TextLineDataset(filepath),
+        lambda filepath: tf.data.experimental.make_csv_dataset(filepath),
         cycle_length=n_readers)
     dataset = dataset.shuffle(5)
     print(dataset)
     for x in dataset:
-      print(x)
+        print(x)
     
     # dataset = dataset.map(parse_fn,num_parallel_calls=2)
 

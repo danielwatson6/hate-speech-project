946171ac32bb358098a5c8501fcf7e8b3961f25e
mdmjg <mdj308@nyu.edu>
2020-02-15 22:04:15 +0400

    nice plots

--- scripts/ambiguity/compare.py
+++ scripts/ambiguity/compare.py
@@ -7,18 +7,26 @@ import pandas as pd
 from scipy.stats import pearsonr, spearmanr
 
 
+def get_floats(line):
+    return line.strip().split(", ")
+
+
 def get_ambiguity(save_dir, data_loader):
     """Run the `ambiguity` method for the specified pretrained model and data loader."""
     # Capture the stdout into a variable. 0 max_seq_len will avoid truncation.
-    proc = subprocess.Popen(
-        f"python run.py ambiguity {save_dir} {data_loader} --max_seq_len=0".split(),
-        stdout=subprocess.PIPE,
-    )
+    command = f"python run.py ambiguity {save_dir} {data_loader} --max_seq_len=0"
+
+    # TODO: get this outta here
+    if not save_dir.startswith("wordnet"):
+        command += " --vocab_path=data/wikitext-2/wiki.vocab.tsv"
+    print(command)
+
+    proc = subprocess.Popen(command.split(), stdout=subprocess.PIPE,)
     output = proc.communicate()[0].decode("utf8")
     # Now parse the printed values.
     ragged = []
     for line in output.split("\n"):
-        ragged.append([float(x) for x in line.split(", ")])
+        ragged.append([float(x) for x in get_floats(line) if line])
     return ragged
 
 
@@ -55,9 +63,10 @@ if __name__ == "__main__":
     if data_loader == "ambiguity":
         # ambiguity for language model and wordnet, ambiguities array 3D, per word at inner most level, per sentence, outermost level is the language model
         df = pd.read_csv(os.path.join("data", "ambiguity.clean.csv"))
+        model_length = df.shape[0]
 
         human_ambiguities = []
-        for sequence in df.iterrows():
+        for i, sequence in df.iterrows():
             index = sequence["index"]
             human_ambiguity = sequence["rating"]
             human_ambiguities.append(human_ambiguity)
@@ -65,7 +74,7 @@ if __name__ == "__main__":
         model_ambiguities = []
         for i, model in enumerate(ambiguities):
             model_ambiguities.append([])
-            for j, sentence in enumerate(model):
+            for j, sentence in enumerate(model[:model_length]):
                 index = df.iloc[j]["index"]
                 ambiguity = sentence[index]
                 model_ambiguities[i].append(ambiguity)
@@ -78,5 +87,7 @@ if __name__ == "__main__":
         for j, x2 in enumerate(ambiguities[i + 1 :], i + 1):
             x1 = [x for sentence in x1 for x in sentence]
             x2 = [x for sentence in x2 for x in sentence]
+
+            print(i, j)
 
             plot_comparison(x1, x2, xlabel=axis_labels[i], ylabel=axis_labels[j])

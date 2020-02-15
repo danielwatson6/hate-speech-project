73ec106eb9fbcb60c71524df1216da823893fa30
mdmjg <mdj308@nyu.edu>
2020-02-15 20:24:53 +0400

    finished compare script

--- scripts/ambiguity/compare.py
+++ scripts/ambiguity/compare.py
@@ -8,8 +8,8 @@ from scipy.stats import pearsonr, spearmanr
 
 
 def get_ambiguity(save_dir, data_loader):
-    # Run the `ambiguity` method for the specified pretrained model and data loader,
-    # capturing the stdout into a variable. 0 max_seq_len will avoid truncation.
+    """Run the `ambiguity` method for the specified pretrained model and data loader."""
+    # Capture the stdout into a variable. 0 max_seq_len will avoid truncation.
     proc = subprocess.Popen(
         f"python run.py ambiguity {save_dir} {data_loader} --max_seq_len=0".split(),
         stdout=subprocess.PIPE,
@@ -20,6 +20,21 @@ def get_ambiguity(save_dir, data_loader):
     for line in output.split("\n"):
         ragged.append([float(x) for x in line.split()])
     return ragged
+
+
+def plot_comparison(x1, x2, xlabel="", ylabel=""):
+    pcc = pearsonr(x1, x2)
+    scc = spearmanr(x1, x2)
+
+    plt.title(
+        "Ambiguities (pcc={:.4f} (p={:.4f}) scc={:.4f} (p={:.4f}))".format(
+            *(pcc + scc)
+        )
+    )
+    plt.plot(x1, x2, "o", color="black", markersize=2)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.savefig(os.path.join("plots", f"{xlabel}_vs_{ylabel}.png"))
 
 
 if __name__ == "__main__":
@@ -37,11 +52,11 @@ if __name__ == "__main__":
     for save_dir in save_dirs:
         ambiguities.append(get_ambiguity(save_dir, data_loader))
 
+    axis_labels = ["wordnet"] + save_dirs
+
     if data_loader == "ambiguity":
         # ambiguity for language model and wordnet, ambiguities array 3D, per word at inner most level, per sentence, outermost level is the language model
         df = pd.read_csv(os.path.join("data", "ambiguity.clean.csv"))
-
-        ambiguities_per_word = []
 
         human_ambiguities = []
         for sequence in df.iterrows():
@@ -49,38 +64,21 @@ if __name__ == "__main__":
             human_ambiguity = sequence["rating"]
             human_ambiguities.append(human_ambiguity)
 
-        word_net = []
-        language_model = []
-        for m, model in enumrate(ambiguities):
-            for i, sentence in enumerate(model):
-                index = df.iloc[i]["index"]
+        model_ambiguities = []
+        for i, model in enumerate(ambiguities):
+            model_ambiguities.append([])
+            for j, sentence in enumerate(model):
+                index = df.iloc[j]["index"]
                 ambiguity = sentence[index]
-                if m == 0:
-                    word_net.append(ambiguity)  # first model is the word_net
-                else:
-                    language_model.append(ambiguity)
+                model_ambiguities[i].append(ambiguity)
 
-                # compare rating and model rating
-
-                ...
+        for mambg, ylabel in zip(model_ambiguities, axis_labels):
+            plot_comparison(human_ambiguities, mambg, xlabel="human", ylabel=ylabel output_path=None)
 
     # Correlation plots between all the distinct pairs of models.
-    save_dirs = ["wordnet"] + save_dirs
     for i, x1 in enumerate(ambiguities):
         for j, x2 in enumerate(ambiguities[i + 1 :], i + 1):
             x1 = [x for sentence in x1 for x in sentence]
             x2 = [x for sentence in x2 for x in sentence]
 
-            # Correlation coefficients with p values.
-            pcc = pearsonr(x1, x2)
-            scc = spearmanr(x1, x2)
-
-            plt.title(
-                "Ambiguities (pcc={:.4f} (p={:.4f}) scc={:.4f} (p={:.4f}))".format(
-                    *(pcc + scc)
-                )
-            )
-            plt.plot(x1, x2, "o", color="black", markersize=2)
-            plt.xlabel(save_dirs[i])
-            plt.ylabel(save_dirs[j])
-            plt.show()
+            plot_comparison(x1, x2, xlabel=axis_labels[i], ylabel=axis_labels[j], output_path=None)

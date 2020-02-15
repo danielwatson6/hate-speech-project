403f48c99ddfb2e94ab27bc6698fce4e689dde4a
Abdullah Zameek <arz268@nyu.edu>
2020-02-15 21:43:41 +0400

    added

--- data_loaders/youtube_comments.py
+++ data_loaders/youtube_comments.py
@@ -10,6 +10,11 @@ import boilerplate as tfbp
 
 num_files = 119
 
+
+def make_csv_dataset(path):
+    return tf.data.experimental.make_csv_dataset(path, 1, num_epochs=1, shuffle=False, select_columns=14)
+
+
 @tfbp.default_export
 class Ambiguity(tfbp.DataLoader):
     default_hparams = {
@@ -17,10 +22,9 @@ class Ambiguity(tfbp.DataLoader):
         "punctuation": True,
         "lowercase": True,
         "max_seq_len": None,
+        "num_examples": 1000,
     }
-    def make_csv_dataset(path):
-        return tf.data.experimental.make_csv_dataset(path, batch_size=1, num_epochs=1, select_columns=14)
-
+    
     def call(self):
         path = os.path.join(os.environ["DATASETS"], "youtube_right")
         channels = list(os.listdir(path))
@@ -28,21 +32,17 @@ class Ambiguity(tfbp.DataLoader):
         for channel in channels:
             channel_paths.append(os.path.join(path, channel))
 
-        filepath_dataset = tf.data.Dataset.list_files(channel_paths)
+        filepath_dataset = tf.data.Dataset.list_files(channel_paths, shuffle=False)
     
         dataset = filepath_dataset.interleave(
             make_csv_dataset,
             cycle_length=default_hparams.batch_size,
             block_length=num_files)
         
-        # dataset = tf.data.experimental.make_csv_dataset(
-        #     os.path.join("data", "ambiguity.clean.csv"),
-        #     self.hparams.batch_size,
-        #     num_epochs=1,
-        #     shuffle=False,
-        #     num_rows_for_inference=None,
-        # )
-
+        if self.hparams.num_examples:
+            dataset = dataset.take(self.hparams.num_examples)
+        
+        dataset = dataset.batch(self.hparams.batch_size)
         dataset = dataset.map(self._dict_to_tensor)
         return dataset.prefetch(1)
 

63eb306ee958acb480be4538c46a8d601010d0dd
Abdullah Zameek <arz268@nyu.edu>
2020-02-15 22:08:06 +0400

    added

--- data_loaders/youtube_comments.py
+++ data_loaders/youtube_comments.py
@@ -12,11 +12,13 @@ num_files = 119
 
 
 def make_csv_dataset(path):
-    return tf.data.experimental.make_csv_dataset(path, 1, num_epochs=1, shuffle=False, select_columns=14)
+    return tf.data.experimental.make_csv_dataset(
+        path, 1, num_epochs=1, shuffle=False, select_columns=14
+    )
 
 
 @tfbp.default_export
-class Ambiguity(tfbp.DataLoader):
+class YouTube(tfbp.DataLoader):
     default_hparams = {
         "batch_size": 32,
         "punctuation": True,
@@ -24,7 +26,7 @@ class Ambiguity(tfbp.DataLoader):
         "max_seq_len": None,
         "num_examples": 1000,
     }
-    
+
     def call(self):
         path = os.path.join(os.environ["DATASETS"], "youtube_right")
         channels = list(os.listdir(path))
@@ -33,18 +35,19 @@ class Ambiguity(tfbp.DataLoader):
             channel_paths.append(os.path.join(path, channel))
 
         filepath_dataset = tf.data.Dataset.list_files(channel_paths, shuffle=False)
-    
+
         dataset = filepath_dataset.interleave(
             make_csv_dataset,
             cycle_length=default_hparams.batch_size,
-            block_length=num_files)
-        
+            block_length=num_files,
+        )
+
         if self.hparams.num_examples:
             dataset = dataset.take(self.hparams.num_examples)
-        
+
         for x in dataset:
             print(x)
-            
+
         dataset = dataset.batch(self.hparams.batch_size)
         dataset = dataset.map(self._dict_to_tensor)
         return dataset.prefetch(1)

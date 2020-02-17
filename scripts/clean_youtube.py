d955417a37584b582a05830eba3160e743c582bd
Abdullah Zameek <arz268@nyu.edu>
2020-02-17 21:47:25 +0400

    added

--- scripts/clean_youtube.py
+++ scripts/clean_youtube.py
@@ -53,19 +53,20 @@ if __name__ == "__main__":
     filepath_dataset = tf.data.Dataset.list_files(channel_paths, shuffle=False)
 
     # filepath_dataset = make_csv_dataset(channel_paths)
-    for x in filepath_dataset:
-        print(x)
-
-    dataset = filepath_dataset.interleave(
-        lambda x : tf.map_fn(make_csv_dataset, x),  
-        cycle_length=32, 
-        block_length=119,) 
+    # for x in filepath_dataset:
+    #     print(x)
+    print(type(filepath_dataset))
     
-    for x in dataset:
-        print(x)
+    # dataset = filepath_dataset.interleave(
+    #     lambd(make_csv_dataset, x),  
+    #     cycle_length=32, 
+    #     block_length=119,) 
+    
+    # for x in dataset:
+    #     print(x)
 
-    dataset = dataset.batch(32)
-    dataset = dataset.map(_dict_to_tensor(dataset))
+    # dataset = dataset.batch(32)
+    # dataset = dataset.map(_dict_to_tensor(dataset))
 
     # for x in dataset:
     #     print(x)

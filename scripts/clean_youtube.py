ba66e5ab2c6d7d399c32372c6aba3f7324175352
Abdullah Zameek <arz268@nyu.edu>
2020-02-17 21:54:47 +0400

    added

--- scripts/clean_youtube.py
+++ scripts/clean_youtube.py
@@ -59,7 +59,7 @@ if __name__ == "__main__":
     # print(type(filepath_dataset))
 
     dataset = filepath_dataset.interleave(
-        lambda x: tf.data.TextLineDataset(x).tf.map_fn(
+        lambda x: tf.data.TextLineDataset(x).map_fn(
             make_csv_dataset, num_parallel_calls=1
         ),
         cycle_length=32,
